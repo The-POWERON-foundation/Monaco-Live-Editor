8d67af1024572b9ad76474f73c2aaf79eb1ef510
Infinite Coder <131605571+markusprograms@users.noreply.github.com>
2025-05-20 22:42:30 +0200

    Fixed documentation
     - Replaced '@the-poweron-foundation/monaco-live-editor' with 'monaco-live-editor'

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const httpServer = http.createServer(expressServer); 
 
 const path = require("path");
-const MonacoLiveEditor = require("../src/index"); // After installing the package, you can replace it with '@the-poweron-foundation/monaco-live-editor'
+const MonacoLiveEditor = require("../src/index"); // After installing the package, you can replace it with 'monaco-live-editor'
 
 expressServer.use(express.static(path.join(__dirname, "public"))); // Serve static files from the public directory
 
